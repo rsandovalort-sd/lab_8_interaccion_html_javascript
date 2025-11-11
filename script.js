f687475951d7cb5dd1e624b6545e3b6771cab5f0
ricardoSo-sd <rsandovalort@gmail.com>
2025-11-11 18:05:53 -0500

    Primer ejercicio completado

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function registrarVoto(){
     const radios = document.getElementsByName("opcion");
     let seleccion = null;
-
+    let votos = parseInt(document.getElementById("votos-js").innerHTML) || 0;
     for( let radio of radios){
         if(radio.checked){
             seleccion = radio.value;
-            alert(seleccion);
-            break;
+            // alert("¡Gracias por tu voto!");
+            votos += 1;
+            document.getElementById("votos-js").innerHTML = votos;
+            break
+        } 
+        
     }
+    if (votos % 5 === 0){
+        console.log("El total de votos es: ",votos); 
+    } 
+}
+   
 
-}
-}
